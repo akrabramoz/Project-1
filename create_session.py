82bebeb6fe01050221fb0a8cdac309b92aa6084c
akrabramoz <akrabramoz@gmail.com>
2025-03-28 07:35:37 +0100

    Update create_session.py

--- create_session.py
+++ create_session.py
@@ -1,12 +1,14 @@
-import os
 from pyrogram import Client
 
-api_id = 20428083 # استبدله بالـ API_ID الخاص بك
-api_hash = "c2c7f4fd4c392d80f859466a73b677f5"  # استبدله بالـ API_HASH الخاص بك
-
-session_string = os.getenv("SESSION_STRING")  # جلب الجلسة من GitHub Secrets
-
-app = Client(":memory:", api_id=api_id, api_hash=api_hash, session_string=session_string)
+app = Client("my_session")
 
 app.start()
-print("✅ تم تسجيل الدخول بنجاح!")
+app.send_message("me", "Session started successfully!")
+app.stop()
+
+# حفظ الجلسة في ملف
+app.export_session_string()  # استخراج الجلسة كسلسلة
+with open("session.txt", "w") as file:
+    file.write(app.export_session_string())
+
+print("تم حفظ الجلسة في session.txt")
